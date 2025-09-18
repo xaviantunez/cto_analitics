0900d9f41798fb36a0f4fa7cd79cadaf285b64cd
xaviantunez <47146622+xaviantunez@users.noreply.github.com>
2025-09-18 17:15:47 +0200

    Add files via upload

--- cto_analitics.js
+++ cto_analitics.js
@@ -1,3 +1,39 @@
+const alevinB = [
+    'Ivor', 'Alexis', 'Aleix', 'Arnau_GE', 'Carles', 'Dario',
+    'Gabriel', 'Hector', 'Joan',
+    'Maia', 'Marc_Iborra','Oscar', 'Andreu', 'Arnau_GLL', 'Brais', 'Carlos',
+    'Guillem',  'Leo',
+    'Mateo', 'Miguel',
+    'Victor'];
+const alevinA = [
+    'Pablo','Ianis','Dani_Lopez', 'Dani_Ortega','Cristian','Maxim', 'Ruben','Andreu', 'Brais',
+    'Hugo', 'Manel', 'Marc_Colom', 'Mateo', 'Guillem', 'Leo', 'Arnau_GLL','Carlos','Victor'
+];
+
+const benjaminA = [
+];
+const benjaminB = ["Adomas Volskis", "Saul_Costa", "Sergi_Climent", "Mario_Mas", "Oliver_Mas", "Leo_Barreres", "Marti_Veiguela",
+    "Joan_Barber","Marco_Gomez","Edu_Mena","Aritz_Salinas","Leo_Perez"
+];
+const alevinF = [
+];
+const prebenjaminA = ["Romeo_Morganti",
+    "Leo_Pérez","Aitor_Martin","Andreu_Romero",
+    "Marti_Romero","Joan_Colom","Iulian_Moldovan","Marco_Valbuena",
+    "Julen_Mena","Artur_Vicens","Sergio_Costa","Jaume_Millet","Patrice_Carbila",
+    "Joan_Ponsoda"
+];
+const infantilA = [];
+const infantilB = [];
+const infantilF = [];
+const cadeteA = [];
+const cadeteB = [];
+const cadeteF = [];
+const juvenilA = [];
+const juvenilB = [];
+const juvenilF = [];
+const equips =["prebenjaminA","benjaminB","benjaminA","alevinF","alevinB","alevinA","infantilA","infantilB","infantilF","cadeteF","cadeteB","cadeteA","juvenilA","juvenilB","juvenilF"];
+
 $(document).ready(function() {
     var myBarChart;
     var first=true;
@@ -26,7 +62,7 @@ $(document).ready(function() {
 
     equips.forEach(function(nombre, index) {
         //alert(nombre);
-        //$("#equipos").append("<option value='"+nombre+"'>"+nombre+"</option>");
+        $("#equipos").append("<option value='"+nombre+"'>"+nombre+"</option>");
     });
 
 
