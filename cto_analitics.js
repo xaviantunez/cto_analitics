7cc8affc83b4af32ed68a50a46f06a809657e0bf
xaviantunez <47146622+xaviantunez@users.noreply.github.com>
2026-03-03 23:13:00 +0100

    Update cto_analitics.js

--- cto_analitics.js
+++ cto_analitics.js
@@ -357,7 +357,7 @@ $(document).ready(function() {
             storageManager("guardar",'checkeados', arraycheckeados);
             intervalos[jugador] = setInterval(function() {
 				//if(jugador=="Julia_Garcia") console.log(jugador+" "+tiempos[jugador]);
-            //tiempos[jugador]++;
+            tiempos[jugador]++;
             showTimeColor(tiempos[jugador],jugador);
             const minutos = Math.floor(tiempos[jugador] / 60);
             const segundos = tiempos[jugador] % 60;
@@ -368,7 +368,7 @@ $(document).ready(function() {
 					//###sugerencia
                     tiempos[jugador]+=Math.floor(Date.now() / 1000)-marcatiempo[jugador];
                 }
-				else tiempos[jugador]++;
+				//else tiempos[jugador]++;
 				//if(jugador=="Julia_Garcia") console.log(jugador+" 2 "+tiempos[jugador]);
                 storageManager("guardar",'tiempos', tiempos);
                 marcatiempo[jugador] = Math.floor(Date.now() / 1000);
@@ -1594,6 +1594,7 @@ $(document).ready(function() {
     $(window).scrollTop(0);
 });       
      //-------------------------------------------------------------------------------------
+
 
 
 
