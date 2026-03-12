33064b80dee278b37c3daa5fdb05eb72fa5a03f2
xaviantunez <47146622+xaviantunez@users.noreply.github.com>
2026-03-12 14:13:51 +0100

    Update equipos.js

--- equipos.js
+++ equipos.js
@@ -1,52 +1,55 @@
+
 const alevinB = [
     'Ivor_Fuentes','Alexis_Garrigos', 'Aleix_Ciscar', 'Arnau_GE', 'Carles_Choez', 'Dario_Moreno',
-    'Gabriel_Ruzafa', 'Hector_Collado', 'Joan_Rueda', 'Maia_Miras', 'Marc_Iborra','Oscar_Tebar', 'Andreu_Perez',
-    'Arnau_GLL', 'Brais_Girau', 'Carlos_Cortes', 'Guillem_Ortola', 'Leo_Antunez', 'Mateo_Virgili', 'Miguel_Monrabal',
-    'Victor_Ibiza','Didac_Carmona','Miguel_Angel_Arjona'];
+    'Gabriel_Ruzafa', 'Hector_Collado', 'Joan_Rueda', 'Maia_Miras', 'Marc_Iborra','Oscar_Tebar', 
+    'Arnau_GLL', 'Carlos_Cortes', 'Leo_Antunez', 'Miguel_Monrabal', 'Victor_Ibiza',
+    'Andreu_Perez','Brais_Girau','Guillem_Ortola', 'Mateo_Virgili','Didac_Carmona','Miguel_Angel_Arjona'];
 
 const alevinA = ['Pablo_Munoz','Ianis_Lungu','Dani_Lopez', 'Dani_Ortega','Cristian_Firici','Maxim_Moran', 'Ruben_Escriva','Andreu_Perez',
     'Brais_Girau', 'Hugo_Martin', 'Manel_Fuster', 'Marc_Colom', 'Mateo_Virgili', 'Guillem_Ortola', 'Leo_Antunez',
-    'Arnau_GLL','Carlos_Cortes','Victor_Ibiza'
-];
+    'Arnau_GLL','Carlos_Cortes','Victor_Ibiza'];
 
-const benjaminA = ["Edgar_Bertomeu","Oscar_Moreno", "Jordi_Oltra", "Alex_Lledo", "Daniel_Matel",  "Joan_Caldes",
+const benjaminA = ["Edgar_Bertomeu","Oscar_Moreno", "Erik_Utrilla", "Jordi_Oltra", "Alex_Lledo", "Daniel_Matel",  "Joan_Caldes",
     "Guillem_Server", "Marc_Gomar", "Gonzalo_Llopis", "Joan_Vidal", "Marc_Sigalat", "Xavi_Riera", "Xavi_Arlandis",
     "Albert_Barreres"];
 
 const benjaminB = ["Adomas_Volskis", "Saul_Costa", "Sergi_Climent", "Mario_Mas", "Oliver_Mas", "Leo_Barreres", "Marti_Veiguela",
-     "Joan_Barber", "Marco_Gomez", "Aritz_Salinas", "Leo_Perez"
-];
+     "Joan_Barber", "Marco_Gomez", "Edu_Mena", "Aritz_Salinas", "Leo_Perez"];
+
 const alevinF = ["Sofia_Escriva", "Julia_Garcia", "Carlota_Esteve", "Martina_Ciscar", "Julieta_Robles", "Ana_Lleches",  "Carmen_Ferrando",
     "Emma_Vicens", "Mya_Mas", "Anna_Iturriaga", "Lola_Iturriaga", "Martina_Benet", "Neus_Mestre",
-    "Aitana_Campos", "Ambar_Cinquemani", "Francesca_Meriano"
-];
+    "Aitana_Campos", "Ambar_Cinquemani", "Francesca_Meriano","Dafne"];
+
 const prebenjaminA = ["Romeo_Morganti", "Leo_Perez", "Aitor_Martin", "Andreu_Romero",
     "Marti_Romero", "Joan_Colom", "Iulian_Moldovan", "Marco_Valbuena",
-    "Julen_Mena", "Artur_Vicens","Sergio_Costa", "Jaume_Millet", "Patrice_Carbila", "Joan_Ponsoda"
-];
+    "Julen_Mena", "Artur_Vicens","Sergio_Costa", "Jaume_Millet", "Patrice_Carbila", "Joan_Ponsoda"];
+
 const infantilA = ["David_Llopis","Rodrigo_Llopis","Raul_Doca","Vicent_Escriva","Guillem_Millet","Andreu_Benet",
     "Ian_Hadj-Akli","Bacari_Traore","Diego_Sanjuan","Marc_Cuevas","Xavi_Choez","Alex_Ayet","Marc_Girau", "Ferran_Fuster",
-    "Quim_Santonja","Sergi_Pinargore",'Dani_Ortega','Dani_Lopez','Cristian_Firici','Ruben_Escriva'];
+    "Quim_Santonja","Sergi_Pinargore",'Dani_Ortega','Dani_Lopez','Cristian_Firici','Ruben_Escriva','Ianis_Lungu'];
+
 const infantilB = [];
+
 const infantilF = [];
+
 const cadeteA = ["Pol_Cremades","Enzo_Ciscar","Ismael_Just","Cristian_Barbu","Alvaro_Cardona","Mateo_Sabater","Sandro_Bosca","Gerard_Mauri",
     "Joan_Molto","Albert_Banuls","Marc_Antunez","Marti_Garcia","Miguel_Cabanas","Llorens_Perez","Joan_Morell",
     "Josep_Savall","Marc_Vilanova","Pere_Minguez","Joan_Segui","Eric_Popa"];
+
 const cadeteB = [];
+
 const cadeteF = ["Neus_Martinez","Anna_Ardid","Nadia_Calvo","Mar_Miras","Clara_Mayans","Macarena_Gomar","Paula_Soler",
     "Ainhoa_Benavent","Alma_Chamorro","Umma_Lazo","Nerea_Ibiza","Maria_Girau","Laia_Calabria","Maria_Liron","Ainara_Gilabert",
     "Marta_Tomas","Neus_Tomas","Mar_Gonzalez","Briana_Valverde","Aitana_Escriva","April_Tablante"];
-const juvenilA = ["Hector_Gosp","Toshko","Haitem_Ettafy","Mohamed_Adim",,"Blai_Fuster","Iker_Fernandez","Carles_Iturriaga",
+
+const juvenilA = ["Hector_Gosp","Toshko","Haitem_Ettafy","Mohamed_Adim","Blai_Fuster","Iker_Fernandez","Carles_Iturriaga",
     "Diego_Fuster","Javier_Ferrando","Iker_Diaz","Antoni_Banuls","Eric_Mestre","Iannis_Saceanu","Oscar_Vaz",
     "Carlos_Sendra","Carles_Carbonell","Spencer_Adams","Antoni_Doumbia","Miquel_Sanchis","Roger_Fenollar","Ovidiu_Costin",
     "Marc_Tomas","Blai_Lopez","Fernando_Guevara","Alexandre_Mestre","Daniel_Bertomeu"];
+
 const juvenilB = [];
+
 const juvenilF = [];
+
 //const equips =["prebenjaminA","benjaminB","benjaminA","alevinF","alevinB","alevinA","infantilA","infantilB","infantilF","cadeteF","cadeteB","cadeteA","juvenilA","juvenilB","juvenilF"];
 const equips =["prebenjaminA","benjaminB","benjaminA","alevinF","alevinB","alevinA","infantilA","cadeteF","cadeteA","juvenilA"];
-
-
-
-
-
-
